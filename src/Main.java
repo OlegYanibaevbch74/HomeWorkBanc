5438a07a9b8be4cbdcb9672fff755f4001c1af32
Олег Яныбайевич <davletoleg@yandex.ru>
2022-12-14 20:17:22 +0500

    начало

--- src/Main.java
+++ src/Main.java
@@ -8,20 +8,5 @@ public class Main {
         if (clientOS1 == 0) {
             System.out.println("установите версию приложения для iOS по ссылке");
         } else System.out.println("Установите версию приложения для Android  по ссылке");
-
-        System.out.println("Задание 2");
-
-        int clientOS = 0; //Что бы правильно предложить для использования нашего продукта просим вас ответить
-        // на несколько вопросов: какое у Вас установленно системное ПО,нажмите "0" если iOS, или "1" если Android.
-        int clientDeviceYear = 2014;
-        if (clientOS == 0 && clientDeviceYear >= 2015 ){
-            System.out.println("Установите версию приложения для iOS по ссылке");
-        } else {
-            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
-        }
-        if (clientOS == 1 && clientDeviceYear >= 2015){
-            System.out.println("Установите версию приложения для Android  по ссылке");
-        } else {
-            System.out.println("Установите облегченную версию приложения для Android по ссылке");}
     }
 }
