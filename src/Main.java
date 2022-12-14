6116fa3483a8220a2ec309374833aa0cdf1cb456
Олег Яныбайевич <davletoleg@yandex.ru>
2022-12-14 21:39:24 +0500

    зд 2 добил

--- src/Main.java
+++ src/Main.java
@@ -9,24 +9,16 @@ public class Main {
             System.out.println("установите версию приложения для iOS по ссылке");
         } else System.out.println("Установите версию приложения для Android  по ссылке");
 
+
+
         System.out.println("Задание 2");
         int clientOS = 0; //Что бы правильно предложить для использования нашего продукта просим вас ответить
            // на несколько вопросов: какое у Вас установленно системное ПО,нажмите "0" если iOS, или "1" если Android.
-        int clientDeviceYear = 2014; // Укажите пожалуйста гот выпуска вашего телефона (т.е. ПО)
+        int clientDeviceYear = 2008; // Укажите пожалуйста гот выпуска вашего телефона (т.е. ПО)
 
-        case 1:
-
-        if (clientOS == 0 && clientDeviceYear >= 2015 ) {
-            System.out.println("Установите версию приложения для iOS по ссылке");
-        } else {
-            System.out.println("Установите облегченную версию приложения для iOS по ссылке");}
-        break;
-
-        case 2:
-
-        if (clientOS == 1 && clientDeviceYear >= 2015){
-            System.out.println("Установите версию приложения для Android  по ссылке");
-        } else {
-            System.out.println("Установите облегченную версию приложения для Android по ссылке");}
+        if (clientOS == 0 && clientDeviceYear >= 2015 ) System.out.println("Установите версию приложения для iOS по ссылке");
+        else if (clientOS == 0 && clientDeviceYear < 2015 ) System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+                    if (clientOS == 1 && clientDeviceYear >= 2015) System.out.println("Установите версию приложения для Android  по ссылке");
+        else if (clientOS == 1 && clientDeviceYear < 2015) System.out.println("Установите облегченную версию приложения для Android по ссылке");
     }
 }
