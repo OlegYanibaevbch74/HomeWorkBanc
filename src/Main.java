1488361feb1a7798c6606b4c4c016825560b0f40
Олег Яныбайевич <davletoleg@yandex.ru>
2022-12-18 15:21:42 +0500

    зд 4

--- src/Main.java
+++ src/Main.java
@@ -9,16 +9,33 @@ public class Main {
             System.out.println("установите версию приложения для iOS по ссылке");
         } else System.out.println("Установите версию приложения для Android  по ссылке");
 
-
-
         System.out.println("Задание 2");
         int clientOS = 0; //Что бы правильно предложить для использования нашего продукта просим вас ответить
-           // на несколько вопросов: какое у Вас установленно системное ПО,нажмите "0" если iOS, или "1" если Android.
+        // на несколько вопросов: какое у Вас установленно системное ПО,нажмите "0" если iOS, или "1" если Android.
         int clientDeviceYear = 2008; // Укажите пожалуйста гот выпуска вашего телефона (т.е. ПО)
+        if (clientOS == 0 && clientDeviceYear >= 2015)
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        else if (clientOS == 0 && clientDeviceYear < 2015)
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        if (clientOS == 1 && clientDeviceYear >= 2015)
+            System.out.println("Установите версию приложения для Android  по ссылке");
+        else if (clientOS == 1 && clientDeviceYear < 2015)
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
 
-        if (clientOS == 0 && clientDeviceYear >= 2015 ) System.out.println("Установите версию приложения для iOS по ссылке");
-        else if (clientOS == 0 && clientDeviceYear < 2015 ) System.out.println("Установите облегченную версию приложения для iOS по ссылке");
-                    if (clientOS == 1 && clientDeviceYear >= 2015) System.out.println("Установите версию приложения для Android  по ссылке");
-        else if (clientOS == 1 && clientDeviceYear < 2015) System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        System.out.println("Задание 3");
+        int year = 2224;     //Небольшая справка: високосным является каждый четвертый год, но не является каждый сотый.
+        // Также високосным является каждый четырехсотый год.
+        if (year % 4 == 0) System.out.println( + year + " год является високосным.");
+        else if (year % 100 == 0 && year % 400 != 0) System.out.println( + year + " год является високосным.");
+        else System.out.println(+year+" год не является високосным.");
+
+        System.out.println("Задание 4");
+
+        int deliveryDistance = 95;
+        deliveryDistance = deliveryDistance / 20;
+        int deliveryTime = deliveryDistance;
+        System.out.println("Потребуется дней: " + deliveryTime);
+
+
     }
 }
